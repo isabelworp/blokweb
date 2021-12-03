02cac64f4a84e3a41d8fd20e007f2b4ed67eec06
isabelworp <94453248+isabelworp@users.noreply.github.com>
2021-12-03 13:40:34 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,11 @@
 // JavaScript Document
+console.log ("Hoi");
+
+var deButton = document.querySelector("nav button");
+
+deButton.addEventListener("click", toggleMenu);
+
+function toggleMenu(event) {
+  deNav = event.target.parentNode;
+  deNav.classList.toggle("toonMenu");
+}
